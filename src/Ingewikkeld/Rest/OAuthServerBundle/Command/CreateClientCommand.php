b4b46555f8a5fa19ffdbca7fcaa6c6180071b337
Mike van Riel <mike.vanriel@naenius.com>
2013-11-01 17:34:35 +0100

    Added Page-level DocBlocks to all PHP files

--- src/Ingewikkeld/Rest/OAuthServerBundle/Command/CreateClientCommand.php
+++ src/Ingewikkeld/Rest/OAuthServerBundle/Command/CreateClientCommand.php
@@ -1,6 +1,10 @@
 <?php
 /**
- * RestDistribution
+ * Symfony REST Edition.
+ *
+ * @link      https://github.com/Ingewikkeld/symfony-rest-edition
+ * @copyright Copyright (c) 2013-2013 Ingewikkeld
+ * @license   https://github.com/Ingewikkeld/symfony-rest-edition/blob/master/LICENSE MIT License
  */
 
 namespace Ingewikkeld\Rest\OAuthServerBundle\Command;
--- src/Ingewikkeld/Rest/UserBundle/Entity/User.php
+++ src/Ingewikkeld/Rest/UserBundle/Entity/User.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Symfony REST Edition.
+ *
+ * @link      https://github.com/Ingewikkeld/symfony-rest-edition
+ * @copyright Copyright (c) 2013-2013 Ingewikkeld
+ * @license   https://github.com/Ingewikkeld/symfony-rest-edition/blob/master/LICENSE MIT License
+ */
+
 namespace Ingewikkeld\Rest\UserBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
