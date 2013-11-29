4011e09d0fb3c635fe4983d594dd327c6d6cb0c5
Mike van Riel <mike.vanriel@naenius.com>
2013-11-29 12:33:37 +0100

    Add (empty) tests for user and repository

---
+++ src/Ingewikkeld/Rest/UserBundle/Tests/Entity/UserTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Symfony REST Edition.
+ *
+ * @link      https://github.com/Ingewikkeld/symfony-rest-edition
+ * @copyright Copyright (c) 2013-2013 Ingewikkeld<info@ingewikkeld.net>
+ * @license   https://github.com/Ingewikkeld/symfony-rest-edition/blob/master/LICENSE MIT License
+ */
+
+namespace Ingewikkeld\Rest\UserBundle\Tests\Entity;
+
+class UserTest extends \PHPUnit_Framework_TestCase
+{
+
+}
